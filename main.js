073d384e9212495876dd7a754d6843e388b916b2
Viking Zhang <vikingmute@gmail.com>
2017-08-07 17:43:03 +0800

    convert tab to 2 spaces

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const NETEASE_API_URL = 'http://music.163.com/weapi'
 program
 	.version('0.1.3')
 	.usage('[options]')
-  .option('-i, --id [id]', '设置下载歌单专辑或者单曲的 ID')
+	.option('-i, --id [id]', '设置下载歌单专辑或者单曲的 ID')
 	.option('-d, --dir [dir]', '设置下载文件夹路径, 默认为 ./downloads, 请使用绝对地址')
 	.option('-t, --type [type]', '设置下载类型 p 代表 歌单，a 代表专辑， s 代表单曲， 默认为单曲')
 	.parse(process.argv)
@@ -56,23 +56,23 @@ const BARS = {}
 const multi = new MultiProgress(process.stderr)
 
 const NeteaseRequest = (url, query) => {
-  let opts = {
-    mode: 'no-cors',
-    method: 'POST',
-    headers: {
-      'Origin': 'http://music.163.com',
-      'Referer': 'http://music.163.com',
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    credentials: 'include'
-  };
-  opts.body = querystring.stringify(query)
-  return new Promise((resolve, reject) => {
-    fetch(NETEASE_API_URL + url, opts)
-      .then(res => res.json())
-      .then(json => resolve(json))
-      .catch(err => reject(err))
-  })
+	let opts = {
+		mode: 'no-cors',
+		method: 'POST',
+		headers: {
+			'Origin': 'http://music.163.com',
+			'Referer': 'http://music.163.com',
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		credentials: 'include'
+	};
+	opts.body = querystring.stringify(query)
+	return new Promise((resolve, reject) => {
+		fetch(NETEASE_API_URL + url, opts)
+			.then(res => res.json())
+			.then(json => resolve(json))
+			.catch(err => reject(err))
+	})
 }
 
 const getSongInfo = (id) => {
@@ -123,7 +123,7 @@ const downloadFile = (name, url, destPath) => {
 					incomplete: ' ',
 					width: 30,
 					total: len,
-	        	})
+						})
 
 				timer = setTimeout(() => {
 					stream.close()
@@ -157,146 +157,146 @@ const processDownload = (track) => {
 }
 
 const getSong = (id, raw = false, br = 320000) => {
-  id = id.split('.').map(i => parseInt(i))
-  let obj = {
-    'ids': id,
-    'br': br,
-    'csrf_token': ''
-  }
-  let encData = Enc.aesRsaEncrypt(JSON.stringify(obj))
-  if(raw){
-    return NeteaseRequest(`/song/enhance/player/url?csrf_token=`, encData)
-  }
-  return new Promise((resolve, reject) => {
-    NeteaseRequest(`/song/enhance/player/url?csrf_token=`, encData)
-      .then(res => {
-        if(!res.data[0].url){
-          reject({
-            success: false,
-            message: '网易 - 歌曲需要付费或者ID错误!'
-          })
-        }
-        resolve({
-          success: true,
-          url: res.data[0].url
-        })
-      })
-      .catch(err => reject({
-      	err: err,
-        success: false,
-        message: '网易 - 歌曲需要付费或者ID错误!'
-      }))
-  });
+	id = id.split('.').map(i => parseInt(i))
+	let obj = {
+		'ids': id,
+		'br': br,
+		'csrf_token': ''
+	}
+	let encData = Enc.aesRsaEncrypt(JSON.stringify(obj))
+	if(raw){
+		return NeteaseRequest(`/song/enhance/player/url?csrf_token=`, encData)
+	}
+	return new Promise((resolve, reject) => {
+		NeteaseRequest(`/song/enhance/player/url?csrf_token=`, encData)
+			.then(res => {
+				if(!res.data[0].url){
+					reject({
+						success: false,
+						message: '网易 - 歌曲需要付费或者ID错误!'
+					})
+				}
+				resolve({
+					success: true,
+					url: res.data[0].url
+				})
+			})
+			.catch(err => reject({
+				err: err,
+				success: false,
+				message: '网易 - 歌曲需要付费或者ID错误!'
+			}))
+	});
 }
 
 const getAlbum = (id, raw) => {
-  let obj = {
-    'csrf_token': ''
-  };
-  let encData = Enc.aesRsaEncrypt(JSON.stringify(obj));
-  if(raw){
-    return NeteaseRequest(`/v1/album/${id}?csrf_token=`, encData);
-  }
-  return new Promise((resolve, reject) => {
-    NeteaseRequest(`/v1/album/${id}?csrf_token=`, encData)
-      .then(res => {
-        let ab = res.songs;
-        let songList = ab.map(item => {
-          return {
-            id: item.id,
-            name: item.name,
-            needPay: item.fee > 0 ? true : false,
-            offlineNow: item.privilege.st < 0 ? true : false,
-            artists: item.ar,
-            album: {
-              id: res.album.id,
-              name: res.album.name,
-              cover: res.album.picUrl.replace('http://', 'https://') + '?param=250y250',
-              coverBig: res.album.picUrl.replace('http://', 'https://') + '?param=400y400',
-              coverSmall: res.album.picUrl.replace('http://', 'https://') + '?param=140y140',
-            }
-          }
-        });
-        let obj = {
-          success: true,
-          name: res.album.name,
-          id: res.album.id,
-          cover: res.album.picUrl.replace('http://', 'https://') + '?param=250y250',
-          coverBig: res.album.picUrl.replace('http://', 'https://') + '?param=400y400',
-          coverSmall: res.album.picUrl.replace('http://', 'https://') + '?param=140y140',
-          needPay: songList[0].needPay,
-          offlineNow: songList[0].offlineNow,
-          artist: {
-            name: res.album.artist.name,
-            id: res.album.artist.id
-          },
-          songList: songList
-        };
-        resolve(obj);
-      })
-      .catch(err => reject({
-        success: false,
-        message: err
-      }))
-  });
+	let obj = {
+		'csrf_token': ''
+	};
+	let encData = Enc.aesRsaEncrypt(JSON.stringify(obj));
+	if(raw){
+		return NeteaseRequest(`/v1/album/${id}?csrf_token=`, encData);
+	}
+	return new Promise((resolve, reject) => {
+		NeteaseRequest(`/v1/album/${id}?csrf_token=`, encData)
+			.then(res => {
+				let ab = res.songs;
+				let songList = ab.map(item => {
+					return {
+						id: item.id,
+						name: item.name,
+						needPay: item.fee > 0 ? true : false,
+						offlineNow: item.privilege.st < 0 ? true : false,
+						artists: item.ar,
+						album: {
+							id: res.album.id,
+							name: res.album.name,
+							cover: res.album.picUrl.replace('http://', 'https://') + '?param=250y250',
+							coverBig: res.album.picUrl.replace('http://', 'https://') + '?param=400y400',
+							coverSmall: res.album.picUrl.replace('http://', 'https://') + '?param=140y140',
+						}
+					}
+				});
+				let obj = {
+					success: true,
+					name: res.album.name,
+					id: res.album.id,
+					cover: res.album.picUrl.replace('http://', 'https://') + '?param=250y250',
+					coverBig: res.album.picUrl.replace('http://', 'https://') + '?param=400y400',
+					coverSmall: res.album.picUrl.replace('http://', 'https://') + '?param=140y140',
+					needPay: songList[0].needPay,
+					offlineNow: songList[0].offlineNow,
+					artist: {
+						name: res.album.artist.name,
+						id: res.album.artist.id
+					},
+					songList: songList
+				};
+				resolve(obj);
+			})
+			.catch(err => reject({
+				success: false,
+				message: err
+			}))
+	});
 }
 
 const getPlaylist = (id, raw) => {
-  let obj = {
-    id,
-    n: 1000,
-    'csrf_token': ''
-  };
-  let encData = Enc.aesRsaEncrypt(JSON.stringify(obj));
-  if(raw){
-    return NeteaseRequest(`/v3/playlist/detail?csrf_token=`, encData);
-  }
-  return new Promise((resolve, reject) => {
-    NeteaseRequest(`/v3/playlist/detail?csrf_token=`, encData)
-      .then(res => {
-        try {
-          let songList = res.playlist.tracks.map((item, index) => {
-            return {
-              id: item.id,
-              name: item.name,
-              artists: item.ar,
-              needPay: item.fee > 0 ? true : false,
-              offlineNow: res.privileges[index].st < 0 ? true : false,
-              album: {
-                id: item.al.id,
-                cover: item.al.picUrl.replace('http://', 'https://') + '?param=250y250',
-                coverBig: item.al.picUrl.replace('http://', 'https://') + '?param=400y400',
-                coverSmall: item.al.picUrl.replace('http://', 'https://') + '?param=140y140',
-                name: item.al.name
-              }
-            };
-          });
-          let obj = {
-            success: true,
-            name: res.playlist.name,
-            id: id,
-            cover: null,
-            author: {
-              id: res.playlist.creator.userId,
-              name: res.playlist.creator.nickname,
-              avatar: res.playlist.creator.avatarUrl
-            },
-            songList: songList
-          };
-          resolve(obj);
-        } catch (e) {
-          console.log(e);
-          reject({
-            success: false,
-            message: 'your netease playlist id is not correct or data mapping is not correct, try query with raw=true'
-          })
-        }
-      })
-      .catch(err => reject({
-        success: false,
-        message: err
-      }))
-  });
+	let obj = {
+		id,
+		n: 1000,
+		'csrf_token': ''
+	};
+	let encData = Enc.aesRsaEncrypt(JSON.stringify(obj));
+	if(raw){
+		return NeteaseRequest(`/v3/playlist/detail?csrf_token=`, encData);
+	}
+	return new Promise((resolve, reject) => {
+		NeteaseRequest(`/v3/playlist/detail?csrf_token=`, encData)
+			.then(res => {
+				try {
+					let songList = res.playlist.tracks.map((item, index) => {
+						return {
+							id: item.id,
+							name: item.name,
+							artists: item.ar,
+							needPay: item.fee > 0 ? true : false,
+							offlineNow: res.privileges[index].st < 0 ? true : false,
+							album: {
+								id: item.al.id,
+								cover: item.al.picUrl.replace('http://', 'https://') + '?param=250y250',
+								coverBig: item.al.picUrl.replace('http://', 'https://') + '?param=400y400',
+								coverSmall: item.al.picUrl.replace('http://', 'https://') + '?param=140y140',
+								name: item.al.name
+							}
+						};
+					});
+					let obj = {
+						success: true,
+						name: res.playlist.name,
+						id: id,
+						cover: null,
+						author: {
+							id: res.playlist.creator.userId,
+							name: res.playlist.creator.nickname,
+							avatar: res.playlist.creator.avatarUrl
+						},
+						songList: songList
+					};
+					resolve(obj);
+				} catch (e) {
+					console.log(e);
+					reject({
+						success: false,
+						message: 'your netease playlist id is not correct or data mapping is not correct, try query with raw=true'
+					})
+				}
+			})
+			.catch(err => reject({
+				success: false,
+				message: err
+			}))
+	});
 }
 
 const processSingleSong = (id, raw = true) => {
